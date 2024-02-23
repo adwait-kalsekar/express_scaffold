33214cc5cb19a935c578ef7cf41488f30d637387
adwait-kalsekar <kalsekaradwait@gmail.com>
2024-02-23 00:54:27 -0500

    file upload setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from './constants';
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) throw Error('Local File Path not found');
+
+    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    });
+    console.log('File uploaded to cloudinary successfully: ', response.url);
+
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); //remove locally saved temp file
+  }
+};
+
+export { uploadOnCloudinary };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,9 +7,11 @@ export const SERVER_PORT = process.env.SERVER_PORT || 8000;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
 
 export const REFRESH_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-
 export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
